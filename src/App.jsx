197a131e64ac0cd1e847a14738f57c387b1efbce
Shah <shahjalal050505@gmail.com>
2023-08-30 00:14:58 +0600

    todo list css

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,24 @@ function App() {
       <h1>To do list </h1>
       <div className="todo-wrapper">
         <div className="todo-input">
-          <div className="todo-input-items">
+          <div className="todo-input-item">
             <label htmlFor="">Title</label>
             <input type="text" placeholder="What is the task title?" />
           </div>
-          <div className="todo-input-items">
+          <div className="todo-input-item">
             <label htmlFor="">Descriotion</label>
             <input type="text" placeholder="What is the task description?" />
           </div>
           <div className="todo-input-items">
-            <button type="button" className="primary-btn">
+            <button type="button" className="primaryBtn">
               Add
             </button>
           </div>
         </div>
 
         <div>
-          <button type="">Todo</button>
-          <button type="">Comleted</button>
+          <button className="secondaryBtn" type="">Todo</button>
+          <button className="secondaryBtn" type="">Comleted</button>
         </div>
         <div className="todo-list">
           <div className="todo-list-item">
